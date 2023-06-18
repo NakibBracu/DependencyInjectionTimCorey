c7296576f1c4ba0407443db35c205ed71e9ea5ce
NakibBracu <nakibrahamanbracu18@gmail.com>
2023-06-18 18:06:02 +0600

    Till lesson 10 done!

---
+++ Resources_Code/DependencyInjectionDemo/BlazorServerDemo/Data/Demo.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorServerDemo.Data
+{
+    public class Demo
+    {
+        public DateTime StartUpTime { get; init; }
+        // We can change the value of the Property When only and only while instantiating the Object
+        public Demo()
+        {
+            StartUpTime = DateTime.Now; 
+        }
+
+    }
+}
