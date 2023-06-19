daccaeba55807bf73fe97c0aeb2a14368b6a13c3
NakibBracu <nakibrahamanbracu18@gmail.com>
2023-06-19 17:48:19 +0600

    Test Case for second method written!

--- Resources_Code/DependencyInjectionDemo/BlazorServerDemo.Tests/UnitTest1.cs
+++ Resources_Code/DependencyInjectionDemo/BlazorServerDemo.Tests/UnitTest1.cs
@@ -11,14 +11,27 @@ namespace BlazorServerDemo.Tests
             ProcessDemo p = new(t); //cause ProcessDemo also Implement IDemo
             int expected = 29;
             int actual = p.GetDaysInMonth();
+           
             Assert.Equal(expected, actual);
+
+
+        }
+        [Fact]
+        public void GetdaysinMonthTest2()
+        {
+            TestingDemo t = new(DateTime.Parse("2/1/2000"));
+            ProcessDemo p = new(t); //cause ProcessDemo also Implement IDemo
+            int expected = 29;
+
+            int actual2 = p.GetDaysInMonthWay2();
+
+            Assert.Equal(expected, actual2);
 
         }
     }
 
     public class TestingDemo : IDemo {
 
-       
         public DateTime StartUpTime { get ; init ; }
 
         public TestingDemo(DateTime _startUptime)
